8bbbac61e39b3c9fa7a1715aa8851b870620dda1
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 01:59:08 +0700

    flashlmdd: Build android.hardware.keymaster@4.1.vendor

--- device.mk
+++ device.mk
@@ -332,6 +332,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/touch_dev_2nd.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/touch_dev_2nd.idc
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lge_msmnile
