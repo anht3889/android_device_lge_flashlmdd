cc2677ce82c50421c82141a2c2d406870104c8fc
Anh Tran <anh.tran.3889@gmail.com>
2022-04-18 01:58:41 +0700

    flashlmdd: Remove some unused IPACM packages

--- device.mk
+++ device.mk
@@ -305,9 +305,7 @@ $(foreach f,$(wildcard $(DEVICE_PATH)/rootdir/bin/*.sh),\
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IPC router config
 PRODUCT_COPY_FILES += \
