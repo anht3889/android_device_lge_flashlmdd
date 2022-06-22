26c68ccc18be2bc81ba9b263c96cf4be6e8100c1
Anh Tran <anh.tran.3889@gmail.com>
2022-06-23 00:13:20 +0700

    flashlmdd: Build android.hardware.light@2.0-service.lge_msmnile

--- device.mk
+++ device.mk
@@ -338,6 +338,10 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.lge_msmnile
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.lge_msmnile
 
 # Media
